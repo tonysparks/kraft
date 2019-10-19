abeaf31f38e1b5eed86552f08d26749701c15958
tonysparks <anthony.sparks31@gmail.com>
2019-10-19 10:16:09 -0500

    properly culled cube

--- assets/shaders/basicFragment.glsl
+++ assets/shaders/basicFragment.glsl
@@ -1,12 +1,37 @@
 #version 150 core
 
-in vec4 Color;
+in vec3 Normal;
 in vec2 TexCoord;
+in vec3 ToLight;
+in vec3 ToCamera;
 
 out vec4 outColor;
 
 uniform sampler2D tex;
+uniform vec3 lightColor;
+uniform vec3 lightPos;
+uniform float shineDamper;
+uniform float reflectivity;
 
 void main() {
-    outColor = texture(tex, TexCoord) * Color;
+    vec3 unitNormal = normalize(Normal);
+    vec3 unitLight = normalize(ToLight);
+
+    float nDot = dot(unitNormal, unitLight);
+    float brightness = max(nDot, 0.3);
+    vec3 diffuse = brightness * lightColor;
+
+    vec3 unitCamera = normalize(ToCamera);
+    vec3 lightDirection = -unitLight;
+    vec3 reflectedLightDirection = reflect(lightDirection, unitNormal);
+
+    float specularFactor = dot(reflectedLightDirection, unitCamera);
+    specularFactor = max(specularFactor, 0.0);
+
+    float dampedFactor = pow(specularFactor, shineDamper);
+    vec3 finalSpecular = dampedFactor * reflectivity * lightColor;
+
+    
+    vec4 c = texture(tex, TexCoord);    
+    outColor = vec4(diffuse, 1.0) * c + vec4(finalSpecular, 1.0);    
 }
--- assets/shaders/basicVertex.glsl
+++ assets/shaders/basicVertex.glsl
@@ -1,22 +1,28 @@
 #version 150 core
 
 in vec3 position;
-in vec4 color;
+in vec3 normal;
 in vec2 texCoord;
 
-out vec4 Color;
+out vec3 Normal;
 out vec2 TexCoord;
+out vec3 ToLight;
+out vec3 ToCamera;
 
 uniform mat4 transformationMatrix;
 uniform mat4 projectionMatrix;
 uniform mat4 viewMatrix;
+uniform vec3 lightPos;
 
 void main() {
+    vec4 worldPosition = transformationMatrix * vec4(position, 1.0);
+
     gl_Position = projectionMatrix * 
                   viewMatrix * 
-                  transformationMatrix * 
-                  vec4(position, 1.0); 
+                  worldPosition;
 
-    Color = color;
+    Normal = (transformationMatrix * vec4(normal, 0.0)).xyz;
+    ToLight = lightPos - worldPosition.xyz;
+    ToCamera = (inverse(viewMatrix) * vec4(0,0,0,1.0)).xyz - worldPosition.xyz;
     TexCoord = texCoord;    
 }
